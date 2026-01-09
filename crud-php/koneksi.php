5732fbbd9620530d0ea9665aec3f2ff2e03286bf
DhimasTechh <dhimasmilyarder@gmail.com>
2026-01-10 00:54:13 +0700

    update2koneksi

--- crud-php/koneksi.php
+++ crud-php/koneksi.php
@@ -1,8 +1,25 @@
 <?php
-$servername = "sql100.infinityfree.com";
-$username = "if0_40866012";
-$password = "vkoLDVddUT7w";
-$db = "if0_40866012_webdailyjournal";
+date_default_timezone_set('Asia/Jakarta');
+
+$servername = "";
+$username = "";
+$password = "";
+$db = "";
+
+// Cek apakah sedang berjalan di Localhost (Laptop) atau Hosting
+if ($_SERVER['SERVER_NAME'] == 'localhost' || $_SERVER['SERVER_NAME'] == '127.0.0.1') {
+    // === KONEKSI LOKAL (XAMPP) ===
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $db = "webdailyjournal"; // Pastikan nama DB lokal benar
+} else {
+    // === KONEKSI HOSTING (InfinityFree) ===
+    $servername = "sql100.infinityfree.com";
+    $username = "if0_40866012";
+    $password = "vkoLDVddUT7w"; // Password hostingmu 
+    $db = "if0_40866012_webdailyjournal";
+}
 
 $conn = new mysqli($servername, $username, $password, $db);
 
